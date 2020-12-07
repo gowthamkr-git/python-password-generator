36dd04826cc1a1800892a0da637394cd0b84a1f8
gowtham <gowthamkr2@gmail.com>
2020-12-07 22:09:58 +0530

    python password generator

---
+++ password generator.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+print('Hello,welcome to password generator')
+length = int(input('\nEnter the length of the password : '))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = lower + upper + num + symbols
+temp = random.sample(all, length)
+password = "".join(temp)
+print(password)
